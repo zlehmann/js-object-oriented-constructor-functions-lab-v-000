f97869db290679451cef9e68f29d4b93499cf7c6
Zachary Lehmann <zlehmann@gmail.com>
2019-03-18 22:33:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ function Scooter (year, color, model) {
   this.color = color
   this.model = model
 }
+
+function Driver (name, age, experience) {
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
+
+function PickupLocation (address, city) {
+  this.address = address
+  this.city = city
+}
